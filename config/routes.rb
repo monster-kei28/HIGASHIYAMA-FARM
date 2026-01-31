873bb201689907c0130f0810ca0f4c57459a61ca
瀬上圭介 <segamikeisuke@segamikeisukenoMacBook-Air.local>
2026-01-31 23:59:34 +0900

    add:利用規約ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "static_pages#top"
 
+  get "terms", to: "static_pages#terms"
+  get "privacy", to: "static_pages#privacy"
+  get "contact", to: "static_pages#contact"
+  
   resources :reservations, only: %i[new create show destroy] do
     collection do
       get  :search    # 予約確認の入力画面
