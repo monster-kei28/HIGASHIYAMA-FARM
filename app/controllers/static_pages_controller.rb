ec6b37e4fe4b83e584d2b506cf2491fec9aa25e0
瀬上圭介 <segamikeisuke@segamikeisukenoMacBook-Air.local>
2026-03-09 22:19:05 +0900

    update:トップページでも予約可能日を判定できるようにmin/max予約日を追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,13 @@
 class StaticPagesController < ApplicationController
   def top
     @events = CalendarEvent.all
+
+    today = Date.current
+    now = Time.zone.now
+    deadline = Time.zone.local(today.year, today.month, today.day, 17, 0, 0)
+
+    @min_reservable_date = now >= deadline ? today + 2.days : today + 1.day
+    @max_reservable_date = today + 60.days
   end
 
   def terms; end
